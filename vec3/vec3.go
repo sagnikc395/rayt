c9002137a6428730931d52912b3730eeeb274980
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-04 23:19:10 +0530

    src: added auxillary methods

--- vec3/vec3.go
+++ vec3/vec3.go
@@ -1,6 +1,9 @@
 package vec3
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type Vec3 struct {
 	E [3]float64
@@ -40,21 +43,74 @@ func (v *Vec3) Increment(v2 Vec3) *Vec3 {
 	return v
 }
 
-func (v *Vec3) ScalarMult(t float64) *Vec3 {
+func (v *Vec3) ScaleUp(t float64) *Vec3 {
 	v.E[0] *= t
 	v.E[1] *= t
 	v.E[2] *= t
 	return v
 }
 
-func (v *Vec3) ScalarDiv(t float64) *Vec3 {
-	return v.ScalarMult(1 / t)
+func (v *Vec3) ScaleDown(t float64) *Vec3 {
+	return v.ScaleUp(1 / t)
 }
 
-func (v *Vec3) length() float64 {
-	return math.Sqrt(v.lengthSquared())
+func (v *Vec3) Length() float64 {
+	return math.Sqrt(v.LengthSquared())
 }
 
-func (v *Vec3) lengthSquared() float64 {
+func (v *Vec3) LengthSquared() float64 {
 	return v.E[0]*v.E[0] + v.E[1]*v.E[1] + v.E[2]*v.E[2]
+}
+
+// utility functions for vec3
+func (v Vec3) String() string {
+	return fmt.Sprintf("%f %f %f", v.E[0], v.E[1], v.E[2])
+}
+
+//vec3 on vec3 operators
+
+func VecAdd(u *Vec3, v *Vec3) *Vec3 {
+	return &Vec3{
+		[3]float64{u.E[0] + v.E[0], u.E[1] + v.E[1], u.E[2] + v.E[2]},
+	}
+}
+
+func VecSub(u *Vec3, v *Vec3) *Vec3 {
+	return &Vec3{
+		[3]float64{u.E[0] - v.E[0], u.E[1] - v.E[1], u.E[2] - v.E[2]},
+	}
+}
+
+func VecMult(u *Vec3, v *Vec3) *Vec3 {
+	return &Vec3{
+		[3]float64{u.E[0] * v.E[0], u.E[1] * v.E[1], u.E[2] * v.E[2]},
+	}
+}
+
+func ScalarProduct(t float64, v *Vec3) *Vec3 {
+	return &Vec3{
+		[3]float64{t * v.E[0], t * v.E[1], t * v.E[2]},
+	}
+}
+
+func ScalarDiv(t float64, v *Vec3) *Vec3 {
+	return ScalarProduct(1/t, v)
+}
+
+func DotProduct(u *Vec3, v *Vec3) float64 {
+	return u.E[0]*v.E[0] + u.E[1]*v.E[1] + u.E[2]*v.E[2]
+}
+
+func CrossProduct(u *Vec3, v *Vec3) *Vec3 {
+	return &Vec3{
+		[3]float64{
+			u.E[1]*v.E[2] - u.E[2]*v.E[1],
+			u.E[2]*v.E[0] - u.E[0]*v.E[2],
+			u.E[0]*v.E[1] - u.E[1]*v.E[0],
+		},
+	}
+}
+
+func (v *Vec3) UnitVector() *Vec3 {
+	return v.ScaleDown(v.Length())
 }
