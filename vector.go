01e59e9fd7e3d8ed8e0f7e3e67155bd6d1733046
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-07-28 19:48:24 +0530

    src: added vector methods

--- vector.go
+++ vector.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 type Vector struct {
 	X float64
 	Y float64
@@ -14,6 +16,34 @@ func InitVector(e0, e1, e2 float64) *Vector {
 	}
 }
 
-func (v *Vector) Negate()  {
-	v.X * -1,
+func (v *Vector) Negate() Vector {
+	return Vector{
+		v.X * -1,
+		v.Y * -1,
+		v.Z * -1,
+	}
+}
+
+func (v *Vector) PlueEqual(vec2 Vector) {
+	v.X += vec2.X
+	v.Y += vec2.Y
+	v.Z += vec2.Z
+}
+
+func (v *Vector) MultipleEqual(t float64) {
+	v.X *= t
+	v.Y *= t
+	v.Z *= t
+}
+
+func (v *Vector) DivideEqual(t float64) {
+	v.MultipleEqual(1 / t)
+}
+
+func (v *Vector) length() float64 {
+	return math.Sqrt(v.lengthSquared())
+}
+
+func (v *Vector) lengthSquared() float64 {
+	return v.X*v.X + v.Y*v.Y + v.Z*v.Z
 }
