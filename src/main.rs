abb7fbdc1372c5139e33884f045398c7d5ec0712
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-20 19:36:27 +0530

    src: added logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use log::info;
+
 const IMAGE_HEIGHT: u16 = 256;
 const IMAGE_WIDTH: u16 = 256;
 
@@ -6,6 +8,7 @@ fn main() {
 
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
     for j in 0..IMAGE_HEIGHT {
+        info!(target:"rayt_events","\rScanlines remaining: {}", IMAGE_HEIGHT - j);
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
@@ -18,4 +21,6 @@ fn main() {
             print!("{} {} {}\n", ir, ig, ib);
         }
     }
+
+    info!(target:"rayt_events","\rDoen.             \n");
 }
