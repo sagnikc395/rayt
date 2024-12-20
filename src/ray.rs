dd5f387b1be2e0ba004321740ae0279e1243a5f1
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-20 23:41:32 +0530

    src: define the ray class

--- src/ray.rs
+++ src/ray.rs
@@ -1,8 +1,34 @@
-#[derive(Debug,Clone,Copy)]
-pub struct Ray {
+use crate::vec3::Vec3;
 
+pub type Point3 = Vec3;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Ray {
+    orig: Point3,
+    dir: Vec3,
 }
 
 impl Ray {
-    
+    //constructor with parameters
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Self {
+            orig: origin,
+            dir: direction,
+        }
+    }
+
+    //getter for origin and direction
+
+    pub fn origin(&self) -> &Point3 {
+        &self.orig
+    }
+
+    pub fn direction(&self) -> &Vec3 {
+        &self.dir
+    }
+
+    //calc point at param t
+    pub fn at(self, t: f64) -> Point3 {
+        self.orig + self.dir * t
+    }
 }
