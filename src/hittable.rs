c076b3f00ba4acd2bf0f3a271736f84527ad104f
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-21 12:36:45 +0530

    src: added the set_face_normal method

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,6 +1,6 @@
 use crate::{
     ray::{Point3, Ray},
-    vec3::Vec3,
+    vec3::{dot, Vec3},
 };
 
 #[derive(Debug, Clone, Copy)]
@@ -9,9 +9,22 @@ pub struct hit_record {
     pub p: Point3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
 }
 
 // virtual in cpp akin to a interface -> so we define it as a trait
 pub trait hittable {
     fn hit(&self, r: &Ray, ray_tmin: f64, ray_tmax: f64, recc: &mut hit_record) -> bool;
+}
+
+fn set_face_normal(r: Ray, outward_normal: Vec3) {
+    //setting the hit record normal vector
+    //outward normal is assumed to have unit length
+    let front_face = dot(r.direction(), outward_normal) < 0.0;
+    let mut normal = Vec3::new(0.0, 0.0, 0.0);
+    if front_face {
+        normal = outward_normal;
+    } else {
+        normal = -outward_normal;
+    }
 }
