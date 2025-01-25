163ce28b8ef795addfa142de58e0523090b62efc
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-01-25 10:20:14 +0530

    src: cleanup vec3 api

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,5 +1,7 @@
-use std::ops::{AddAssign, Index, MulAssign, Neg};
+use std::format;
+use std::ops::{AddAssign, Index, Neg};
 
+#[derive(Copy, Clone)]
 pub struct Vec3 {
     e: [f64; 3],
 }
@@ -47,6 +49,11 @@ impl Vec3 {
     fn length_squared(self) -> f64 {
         self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
     }
+
+    fn to_string(&self) -> String {
+        let s = format!("{} {} {}", self.e[0], self.e[1], self.e[2]);
+        return s;
+    }
 }
 
 //operators can be overload in rust: ref:https://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/first-edition/operators-and-overloading.html
@@ -71,4 +78,41 @@ impl AddAssign for Vec3 {
         self.e[1] += v.e[1];
         self.e[2] += v.e[2];
     }
+}
+
+// other operators define as normal functions
+pub fn add(u: &Vec3, v: &Vec3) -> Vec3 {
+    return Vec3::new(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + u.e[2]);
+}
+
+pub fn subtract(u: &Vec3, v: &Vec3) -> Vec3 {
+    return Vec3::new(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+pub fn product(u: &Vec3, v: &Vec3) -> Vec3 {
+    return Vec3::new(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+pub fn scale_up(u: &Vec3, t: f64) -> Vec3 {
+    return Vec3::new(u.e[0] * t, u.e[1] * t, u.e[2] * t);
+}
+
+pub fn divide(v: &Vec3, t: f64) -> Vec3 {
+    return scale_up(v, 1 as f64 / t);
+}
+
+pub fn dot_product(u: &Vec3, v: &Vec3) -> f64 {
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+pub fn cross_product(u: &Vec3, v: &Vec3) -> Vec3 {
+    return Vec3::new(
+        u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0],
+    );
+}
+
+pub fn unit_vector(v: &Vec3) -> Vec3 {
+    return divide(v, v.length());
 }
